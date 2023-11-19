b69e36c6ddbc9c5c4a41ff5690fa3837c5f7cea2
Cesar Muñoz <cesar.alberto.munoz@gmail.com>
2023-11-19 09:59:17 +0100

    feat: undo save favourite (#6)

--- lib/favourites.ts
+++ lib/favourites.ts
@@ -7,10 +7,16 @@ type saveFavouriteType = {
   userId: string;
   email: string;
   eventId: string;
+  undo?: boolean;
 };
 
 export async function saveFavourite(request: saveFavouriteType) {
   await dbConnect();
+
+  if(request.undo) {
+    await Favourites.deleteOne({ userId: request.userId, eventId: request.eventId });
+    return;
+  }
 
   const { userId, email, eventId } = request;
   const favourite = new Favourites({
